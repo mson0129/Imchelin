99ff08b37e10b00262225294811680d963c9be9e
Michael Son <mson0129@gmail.com>
2016-09-01 10:46:54 +0900

    v0.1.2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
+console.log('Imchelin is ready.');
 var express = require('express');
 var router = express.Router();
+/*
+var ews = require('ews-javascript-api');
+var mysql = require('mysql');
+var pool = mysql.createPool({
 
-/* GET home page. */
+});
+*/
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+}).post('/', function(req, res, next) {
+  res.send('sry.');
+});
+
+router.get('/pseudo', function(req, res, next) {
+  res.render('pseudo', {headers: req.headers, queryString: req._parsedUrl.query});
 });
 
 module.exports = router;
