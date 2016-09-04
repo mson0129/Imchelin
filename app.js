fcfd33cd093bb8b906682f7cdfff0011c9aead75
Michael Son <mson0129@gmail.com>
2016-09-05 05:22:42 +0900

    v0.1.5

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var routes = require('./routes/index');
+var reviews = require('./routes/reviews');
+var randoms = require('./routes/randoms');
+var suggestions = require('./routes/suggestions');
 var users = require('./routes/users');
 
 var app = express();
@@ -31,6 +34,9 @@ app.use(require('node-sass-middleware')({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/reviews', reviews);
+app.use('/randoms', randoms);
+app.use('/suggestions', suggestions);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
---
+++ routes/suggestions.js
@@ -0,0 +1,9 @@
+console.log('Suggestions App is ready.');
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('tpl', {headers: req.headers, queryString: req._parsedUrl.query});
+})
+
+module.exports = router;
