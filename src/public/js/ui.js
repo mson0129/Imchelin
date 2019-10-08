f0f25427cbb0e2f5f035353d06d85bde41945afe
Michael Son <mson0129@gmail.com>
2019-10-08 23:22:55 +0900

    v0.0.7

--- src/public/js/ui.js
+++ src/public/js/ui.js
@@ -1,34 +1,5 @@
 (function () {
-    //Display Light or Dark Mode
-    const body = document.querySelector('body');
-    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
-    const isLightMode = window.matchMedia('(prefers-color-scheme: light)').matches
-    const isNotSpecified = window.matchMedia('(prefers-color-scheme: no-preference)').matches
-    const hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified;
-
-    window.matchMedia('(prefers-color-scheme: dark)').addListener(e => e.matches && activateDarkMode())
-    window.matchMedia('(prefers-color-scheme: light)').addListener(e => e.matches && activateLightMode())
-
-    function activateDarkMode() {
-        body.classList.add('dark');
-        body.classList.remove('light');
-    }
-
-    function activateLightMode() {
-        body.classList.add('light');
-        body.classList.remove('dark');
-    }
-
-    if(isDarkMode) activateDarkMode()
-    if(isLightMode) activateLightMode()
-    if(isNotSpecified || hasNoSupport) {
-        console.log('You specified no preference for a color scheme or your browser does not support it. I Schedule dark mode during night time.')
-        now = new Date();
-        hour = now.getHours();
-        if (hour < 4 || hour >= 16) {
-            activateDarkMode();
-        }
-    }
+    
 
     //Screen Orientation
     if(typeof(ScreenOrientation.lock) === "function") {
